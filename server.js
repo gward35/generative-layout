fd476ecd8bbdf05afc8970449e059e96bcead46c
Scott Donaldson <scott@isl.co>
2019-09-12 14:18:39 -0400

    browser-sync and reload automatically

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
-const canvas = require('./canvas');
+const canvas = require("./src/canvas");
 
 // app.use(express.static('public'));
-app.get('/', (req, res) => {
-    canvas(res);
-})
+app.get("/", (req, res) => {
+  canvas(res);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ src/canvas.js
@@ -0,0 +1,21 @@
+const { createCanvas } = require("canvas");
+module.exports = function(res) {
+  const canvas = createCanvas(200, 200);
+  const context = canvas.getContext("2d");
+  console.log(canvas);
+
+  function triangle(x1, y1, x2, y2, x3, y3) {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.lineTo(x3, y3);
+    context.closePath();
+    context.fill();
+  }
+
+  context.fillStyle = "red";
+  triangle(0, 100, 100, 100, 100, 0);
+
+  res.set("Content-Type", "image/png");
+  canvas.pngStream().pipe(res);
+};
