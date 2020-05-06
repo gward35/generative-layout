b21280239989a1b7c4e09003aec456e651819dd9
gward35 <g.g.ward90@gmail.com>
2020-05-05 21:37:03 -0400

    cut test

--- public/js/generative.test.js
+++ public/js/generative.test.js
@@ -8,14 +8,6 @@ describe("width, height, tilesize, seed in setup function", () => {
 
   let width = "&width=2000";
 
-  test("width string", () => {
-    expect(widthObj.string).toBe("&width=");
-  });
-
-  test("width value", () => {
-    expect(widthObj.value).toEqual(2000);
-  });
-
   test("complete width value", () => {
     expect(width).toMatch(widthObj.string + widthObj.value);
   });
