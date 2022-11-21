33385fe6fa1beec4fc580041539b51861c47e843
gward35 <g.g.ward90@gmail.com>
2022-11-20 22:07:58 -0500

    revert all

--- server.js
+++ server.js
@@ -55,8 +55,6 @@ const randomizer = () => {
 app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.sendFile(__dirname + "/public/index.html");
   res.postUrl;
 });
